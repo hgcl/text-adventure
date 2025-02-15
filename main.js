cbc7cdac9ded0b6d015b0c0c55d1d8cd2cb7df3c
Clara Le <hey@clarale.com>
2025-02-15 12:32:50 +0700

    refactor: simplify code

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-import { Game, Character } from "./modules/classes.js";
+import { Character } from "./modules/classes.js";
 import { scenes } from "./modules/scenes.js";
 
 /**
  * Define game initial variables
  */
-let game = new Game(scenes.scene1);
 let player = new Character(0, 0, []);
-let scene;
+let scene = scenes.scene1;
 
 const currSceneEl = document.getElementById("curr-scene");
 const currDescriptionEl = document.getElementById("curr-description");
@@ -17,13 +16,12 @@ const inventoryEl = document.getElementById("inventory");
  * Generate new scene
  */
 function newScene() {
-  // Set the scene
-  scene = game.playScene();
+  console.log("===> NEW SCENE: " + scene.location);
 
   currSceneEl.textContent = scene.location;
   currDescriptionEl.textContent = scene.description;
 
-  // Create a button for each choice
+  // Create a button for each choice and record its index number
   for (let i = 0; i < scene.choices.length; i++) {
     let btn = document.createElement("button");
     btn.innerHTML = scene.choices[i].name;
@@ -37,17 +35,20 @@ function newScene() {
  * Click on one of the different choices
  */
 currChoicesEl.addEventListener("click", (e) => {
-  // Get next scene (based on choice index)
+  // Get next scene object (based on choice index)
   const index = e.target.getAttribute("data-index");
   const nextScene = scenes[scene.choices[index].next];
+
   // Console log choice
   console.log("[ Clicked on: " + scene.choices[index].name + " ]");
+
   // Earn points?
   const newPoints = scene.choices[index].points;
   if (newPoints !== undefined) {
     console.log("+" + newPoints + " point(s)");
     player.addPoints(newPoints);
   }
+
   // Gain inventory items?
   const newItems = scene.choices[index].inventory;
   if (newItems !== undefined) {
@@ -55,22 +56,29 @@ currChoicesEl.addEventListener("click", (e) => {
     player.addItem(newItems);
   }
   // Update inventory in UI
-  let par = document.createElement("p");
-  par.innerHTML = newItems.name;
-  inventoryEl.appendChild(par);
-  // Record potential points for item
-  par.setAttribute("data-points", newItems.points);
+  inventoryEl.replaceChildren();
+  player.inventory.forEach((item) => {
+    let par = document.createElement("p");
+    par.innerHTML = item.name;
+    inventoryEl.appendChild(par);
+  });
 
   // Clear all previous choices (buttons)
   currChoicesEl.replaceChildren();
 
   // Set the next scene
   if (nextScene !== undefined) {
-    game = new Game(nextScene);
+    scene = nextScene;
     newScene();
-    console.log("===> NEW SCENE: " + nextScene.location);
   }
 });
 
 newScene();
-console.log("===> START SCENE: " + scene.location);
+
+/**
+ * Make some variables accessible via DevTools
+ */
+
+// window.player = player;
+// window.scene = scene;
+// window.scenes = scenes;
--- modules/classes.js
+++ modules/classes.js
@@ -1,40 +1,18 @@
 /**
- * Game class
- */
-export class Game {
-  constructor(scene) {
-    this._scene = scene;
-  }
-  playScene() {
-    return this._scene;
-  }
-}
-
-/**
  * Character class
  */
 export class Character {
   constructor(age, points, inventory) {
-    this._age = age;
-    this._points = points;
-    this._inventory = inventory;
+    this.age = age;
+    this.points = points;
+    this.inventory = inventory;
   }
   addPoints(point) {
-    this._points = this._points + point;
-    console.log("Total points: " + this._points);
+    this.points = this.points + point;
+    console.log("Total points: " + this.points);
   }
   addItem(item) {
-    this._inventory.push(item);
-    console.log("Full inventory :" + JSON.stringify(this._inventory));
-  }
-}
-
-/**
- * Item class (can be part of the inventory)
- */
-export class Item {
-  constructor(name, points) {
-    this._name = name;
-    this._points = points;
+    this.inventory.push(item);
+    console.log("Full inventory: " + JSON.stringify(this.inventory));
   }
 }
